71b4cc6d22027586ddb7b0ef0c35a64dcd6eb869
cristiano939 <cristiano939@msn.com>
2018-11-15 15:01:11 -0200

    changed objects

--- ListaGrafos/Models/Vertice.cs
+++ ListaGrafos/Models/Vertice.cs
@@ -14,44 +14,7 @@ namespace ListaGrafos.Models
         public int Grau { get; set; }
 
 
-        public bool isAdjacente(Vertice v1, Vertice v2)
-        {
-            bool adj1 = false, adj2 = false;
-            foreach (var aresta in v1.Arestas)
-            {
-                if (aresta.VerticeD == v2.VerticeId)
-                {
-                    adj1 = true;
-                }
-            }
-            foreach (var aresta in v2.Arestas)
-            {
-                if (aresta.VerticeD == v1.VerticeId)
-                {
-                    adj2 = true;
-                }
-            }
-            if (adj1 == true && adj2 == true)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public int getGrau(Vertice v)
-        {
-            int grau = v.Arestas.Count;
-
-            foreach (var arestaChegada in Arestas)
-            {
-                foreach (var arestaSaida in v.Arestas)
-                    if (arestaChegada.VerticeD == v.VerticeId && arestaChegada.VerticeO != arestaSaida.VerticeD)
-                    {
-                        grau += 1;
-                    }
-            }
-            return grau;
-        }
+       
 
     }
 
