09962b4e92ada388399e4f0bc774122517b518f3
cristiano939 <cristiano939@msn.com>
2018-11-17 15:40:20 -0200

    all scenarios on test cases

--- TI.SistemaVoo.Tests/GrafosTests.cs
+++ TI.SistemaVoo.Tests/GrafosTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using Shouldly;
+using System;
 
 namespace TI.SistemaVoo.Tests
 {
@@ -31,15 +32,15 @@ namespace TI.SistemaVoo.Tests
         }
 
 
-        [TestCase("BH","SP")]
-        [TestCase("SP","OSASCO")]
-        [TestCase("RJ","PALMAS")]
+        [TestCase("BH", "SP")]
+        [TestCase("SP", "OSASCO")]
+        [TestCase("RJ", "PALMAS")]
         [TestCase("PALMAS", "PORTO ALEGRE")]
         [TestCase("UBERLANDIA", "BOA VISTA")]
         [TestCase("OSASCO", "NATAL")]
         [TestCase("FORTALEZA", "CURITIBA")]
         [TestCase("SALVADOR", "PORTO ALEGRE")]
-        public void isAtingivel_Test(string origem,string destino)
+        public void isAtingivel_Test(string origem, string destino)
         {
             Grafo Rotas = new Grafo();
             Grafo Voos = new Grafo();
@@ -54,7 +55,90 @@ namespace TI.SistemaVoo.Tests
             vDestino.ShouldNotBeNull();
             vDestino.Aeroporto.ShouldBe(destino);
 
-            Rotas.isAtingivel(vOrigem,vDestino).ShouldBeTrue();
+            Rotas.isAtingivel(vOrigem, vDestino).ShouldBeTrue();
+            return;
+        }
+
+        [TestCase("BH", "SP")]
+        [TestCase("SP", "OSASCO")]
+        [TestCase("RJ", "PALMAS")]
+        [TestCase("PALMAS", "PORTO ALEGRE")]
+        [TestCase("UBERLANDIA", "BOA VISTA")]
+        [TestCase("OSASCO", "NATAL")]
+        [TestCase("FORTALEZA", "CURITIBA")]
+        [TestCase("SALVADOR", "PORTO ALEGRE")]
+        public void MelhorCaminho_Test(string origem, string destino)
+        {
+            Grafo Rotas = new Grafo();
+            Grafo Voos = new Grafo();
+            Rotas.CriaGrafo("rotas.txt");
+            Voos.CriaGrafo("voos.txt");
+
+            var vOrigem = Rotas.EncontraVerticePorNome(origem);
+            var vDestino = Rotas.EncontraVerticePorNome(destino);
+            vOrigem.ShouldNotBeNull();
+            vOrigem.Aeroporto.ShouldBe(origem);
+
+            vDestino.ShouldNotBeNull();
+            vDestino.Aeroporto.ShouldBe(destino);
+
+            Rotas.MelhorCaminho(origem, destino).Count.ShouldBeGreaterThan(0);
+            return;
+        }
+
+        [TestCase("BH", "SP")]
+        [TestCase("SP", "OSASCO")]
+        [TestCase("RJ", "PALMAS")]
+        [TestCase("PALMAS", "PORTO ALEGRE")]
+        [TestCase("UBERLANDIA", "BOA VISTA")]
+        [TestCase("OSASCO", "NATAL")]
+        [TestCase("FORTALEZA", "CURITIBA")]
+        [TestCase("SALVADOR", "PORTO ALEGRE")]
+        public void EncontrarCutSet_Test(string origem, string destino)
+        {
+            Grafo Rotas = new Grafo();
+            Grafo Voos = new Grafo();
+            Rotas.CriaGrafo("rotas.txt");
+            Voos.CriaGrafo("voos.txt");
+
+            var vOrigem = Rotas.EncontraVerticePorNome(origem);
+            var vDestino = Rotas.EncontraVerticePorNome(destino);
+            vOrigem.ShouldNotBeNull();
+            vOrigem.Aeroporto.ShouldBe(origem);
+
+            vDestino.ShouldNotBeNull();
+            vDestino.Aeroporto.ShouldBe(destino);
+
+            Rotas.EncontraCutSet(vOrigem, vDestino).Count.ShouldBeGreaterThan(0);
+            return;
+        }
+
+        [TestCase("2018-11-18 20:05:20","BH", "SP")]
+        [TestCase("2018-11-18 20:05:20", "BH", "OSASCO")]
+        [TestCase("2018-11-18 20:05:20", "SP", "OSASCO")]
+        [TestCase("2018-11-18 20:05:20", "RJ", "PALMAS")]
+        [TestCase("2018-11-18 20:05:20", "PALMAS", "PORTO ALEGRE")]
+        [TestCase("2018-11-18 20:05:20", "UBERLANDIA", "BOA VISTA")]
+        [TestCase("2018-11-18 20:05:20", "OSASCO", "NATAL")]
+        [TestCase("2018-11-18 20:05:20", "FORTALEZA", "CURITIBA")]
+        [TestCase("2018-11-18 20:05:20", "SALVADOR", "PORTO ALEGRE")]
+        public void EncontrarHorarioMaximo_Test(DateTime horarioMax, string origem, string destino)
+        {
+            Grafo Rotas = new Grafo();
+            Grafo Voos = new Grafo();
+            Rotas.CriaGrafo("rotas.txt");
+            Voos.CriaGrafo("voos.txt");
+
+            var vOrigem = Rotas.EncontraVerticePorNome(origem);
+            var vDestino = Rotas.EncontraVerticePorNome(destino);
+            vOrigem.ShouldNotBeNull();
+            vOrigem.Aeroporto.ShouldBe(origem);
+
+            vDestino.ShouldNotBeNull();
+            vDestino.Aeroporto.ShouldBe(destino);
+
+            Rotas.CalculaHoraMaxSaida(horarioMax, origem, destino).ShouldNotBeNull();
+            Rotas.CalculaHoraMaxSaida(horarioMax, origem, destino).ShouldNotBe(DateTime.MinValue);
             return;
         }
 
